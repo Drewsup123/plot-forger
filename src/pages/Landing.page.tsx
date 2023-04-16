5bc905c7ffa2f78336b982840f301d3fb1c12613
Drew <drewsup123c@gmail.com>
2023-04-16 10:23:36 -0600

    boilerplate landing page start

---
+++ src/pages/Landing.page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "../styles/pages/landingPage.module.sass";
+import landingPageRead from "../Assets/images/landingPageRead.jpg";
+import landingPageWrite from "../Assets/images/landingPageWrite.jpg";
+
+const LandingPage = () => {
+    return(
+        <div className={styles.landingPage}>
+            <div className={styles.section + " " + styles.leftSection} style={{
+                backgroundImage: `url(${landingPageRead})`
+            }}>
+                <button>I am a Reader</button>
+            </div>
+            <div className={styles.section + " " + styles.rightSection} style={{
+                backgroundImage: `url(${landingPageWrite})`
+            }}>
+                    <button>I am a Creator</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
