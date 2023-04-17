06f53dee3a4261bdc6f15ff9a8e13a4125087306
Drew <drewsup123c@gmail.com>
2023-04-16 21:49:53 -0600

    selected type boilerplate

--- src/pages/Landing.page.tsx
+++ src/pages/Landing.page.tsx
@@ -4,20 +4,42 @@ import landingPageRead from "../Assets/images/landingPageRead.jpg";
 import landingPageWrite from "../Assets/images/landingPageWrite.jpg";
 
 const LandingPage = () => {
-    return(
+    const [selectedType, setSelectedType] = React.useState<
+        "reader" | "creator" | null
+    >(null);
+
+    const selectType = (
+        e: React.MouseEvent<HTMLButtonElement>,
+        type: "reader" | "creator"
+    ) => {
+        e.preventDefault();
+        setSelectedType(type);
+    };
+
+    return (
         <div className={styles.landingPage}>
-            <div className={styles.section + " " + styles.leftSection} style={{
-                backgroundImage: `url(${landingPageRead})`
-            }}>
-                <button>I am a Reader</button>
+            <div
+                className={styles.section + " " + styles.leftSection}
+                style={{
+                    backgroundImage: `url(${landingPageRead})`,
+                }}
+            >
+                <button onClick={(e) => selectType(e, "reader")}>
+                    I am a Reader
+                </button>
             </div>
-            <div className={styles.section + " " + styles.rightSection} style={{
-                backgroundImage: `url(${landingPageWrite})`
-            }}>
-                <button>I am a Creator</button>
+            <div
+                className={styles.section + " " + styles.rightSection}
+                style={{
+                    backgroundImage: `url(${landingPageWrite})`,
+                }}
+            >
+                <button onClick={(e) => selectType(e, "creator")}>
+                    I am a Creator
+                </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default LandingPage;
